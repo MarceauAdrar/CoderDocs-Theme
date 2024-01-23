495229b5e221bf954ae86986f0d9055885ba831d
MarceauAdrar <marceaurodrigues@adrar-formation.com>
2024-01-23 20:21:06 +0000

    Mise à jour de la documentation

--- assets/js/docs.js
+++ assets/js/docs.js
@@ -1,8 +1,7 @@
 "use strict";
 
-
 /* ====== Define JS Constants ====== */
-const sidebarToggler = document.getElementById('docs-sidebar-toggler');
+// const sidebarToggler = document.getElementById('docs-sidebar-toggler');
 const sidebar = document.getElementById('docs-sidebar');
 const sidebarLinks = document.querySelectorAll('#docs-sidebar .scrollto');
 
@@ -10,45 +9,43 @@ const sidebarLinks = document.querySelectorAll('#docs-sidebar .scrollto');
 
 /* ===== Responsive Sidebar ====== */
 
-window.onload=function() 
-{ 
-    responsiveSidebar(); 
+window.onload = function () {
+	responsiveSidebar();
 };
 
-window.onresize=function() 
-{ 
-    responsiveSidebar(); 
+window.onresize = function () {
+	responsiveSidebar();
 };
 
 
 function responsiveSidebar() {
-    let w = window.innerWidth;
-	if(w >= 1200) {
-	    // if larger 
-	    console.log('larger');
+	let w = window.innerWidth;
+	if (w >= 1200) {
+		// if larger 
+		console.log('larger');
 		sidebar.classList.remove('sidebar-hidden');
 		sidebar.classList.add('sidebar-visible');
-		
+
 	} else {
-	    // if smaller
-	    console.log('smaller');
-	    sidebar.classList.remove('sidebar-visible');
+		// if smaller
+		console.log('smaller');
+		sidebar.classList.remove('sidebar-visible');
 		sidebar.classList.add('sidebar-hidden');
 	}
 };
 
-sidebarToggler.addEventListener('click', () => {
-	if (sidebar.classList.contains('sidebar-visible')) {
-		console.log('visible');
-		sidebar.classList.remove('sidebar-visible');
-		sidebar.classList.add('sidebar-hidden');
-		
-	} else {
-		console.log('hidden');
-		sidebar.classList.remove('sidebar-hidden');
-		sidebar.classList.add('sidebar-visible');
-	}
-});
+// sidebarToggler.addEventListener('click', () => {
+// 	if (sidebar.classList.contains('sidebar-visible')) {
+// 		console.log('visible');
+// 		sidebar.classList.remove('sidebar-visible');
+// 		sidebar.classList.add('sidebar-hidden');
+
+// 	} else {
+// 		console.log('hidden');
+// 		sidebar.classList.remove('sidebar-hidden');
+// 		sidebar.classList.add('sidebar-visible');
+// 	}
+// });
 
 
 /* ===== Smooth scrolling ====== */
@@ -56,27 +53,27 @@ sidebarToggler.addEventListener('click', () => {
 /* Ref: https://github.com/iamdustan/smoothscroll */
 
 sidebarLinks.forEach((sidebarLink) => {
-	
+
 	sidebarLink.addEventListener('click', (e) => {
-		
+
 		e.preventDefault();
-		
+
 		var target = sidebarLink.getAttribute("href").replace('#', '');
-		
+
 		//console.log(target);
-		
-        document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
-        
-        
-        //Collapse sidebar after clicking
-		if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200){
-			
+
+		document.getElementById(target).scrollIntoView({ behavior: 'smooth' });
+
+
+		//Collapse sidebar after clicking
+		if (sidebar.classList.contains('sidebar-visible') && window.innerWidth < 1200) {
+
 			sidebar.classList.remove('sidebar-visible');
-		    sidebar.classList.add('sidebar-hidden');
-		} 
-		
-    });
-	
+			sidebar.classList.add('sidebar-hidden');
+		}
+
+	});
+
 });
 
 
@@ -91,15 +88,4 @@ var spy = new Gumshoe('#docs-nav a', {
 /* ====== SimpleLightbox Plugin ======= */
 /*  Ref: https://github.com/andreknieriem/simplelightbox */
 
-var lightbox = new SimpleLightbox('.simplelightbox-gallery a', {/* options */});
-
-
-
-
-
-
-
-
-
-
-
+var lightbox = new SimpleLightbox('.simplelightbox-gallery a', {/* options */ });
---
+++ index.php
@@ -0,0 +1,168 @@
+<!DOCTYPE html>
+<html lang="en">
+
+<head>
+	<title>FormaDocs - Jouez avec les requêtes et découvrez le principe des APIs</title>
+	<?php include __DIR__ . '/assets/html/head.html'; ?>
+</head>
+
+<body>
+	<?php include __DIR__ . '/assets/html/header.html'; ?>
+
+
+	<div class="page-header theme-bg-dark py-5 text-center position-relative">
+		<div class="theme-bg-shapes-right"></div>
+		<div class="theme-bg-shapes-left"></div>
+		<div class="container">
+			<h1 class="page-heading single-col-max mx-auto">Documentation</h1>
+			<div class="page-intro single-col-max mx-auto">Tout ce que vous cherchez pour l'API REST est ici.</div>
+			<div class="main-search-box pt-3 d-block mx-auto">
+				<form class="search-form w-100">
+					<input type="text" placeholder="Rechercher dans la documentation..." name="search" class="form-control search-input">
+					<button type="submit" class="btn search-btn" value="Search"><i class="fas fa-search"></i></button>
+				</form>
+			</div>
+		</div>
+	</div><!--//page-header-->
+	<div class="page-content">
+		<div class="container">
+			<div class="docs-overview py-5">
+				<div class="row justify-content-center">
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-bullseye"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">Vos missions</span>
+								</h5>
+								<div class="card-text">
+									Cette section recense toutes les missions qui sont attendues de vous.
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-missions"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-map-signs"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">Introduction</span>
+								</h5>
+								<div class="card-text">
+									Explication concice de ce que sont les API. De la signification jusqu'à ce qu'elles permettent de faire en général.
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-introduction"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-box"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">APIs</span>
+								</h5>
+								<div class="card-text">
+									Retrouvez dans cette section toutes les actions que vous pouvez effectuer.
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-api"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-tools"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">Postman</span>
+								</h5>
+								<div class="card-text">
+									Petit guide sur à quoi sert et comment fonctionne le logiciel Postman.
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-postman"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-laptop-code"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">État des services</span>
+								</h5>
+								<div class="card-text">
+									Indicateurs, statistiques, incidents, retrouvez toutes les informations relatives aux performances de l'API ici.
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-status"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+					<div class="col-12 col-lg-4 py-3">
+						<div class="card shadow-sm">
+							<div class="card-body">
+								<h5 class="card-title mb-3">
+									<span class="theme-icon-holder card-icon-holder me-2">
+										<i class="fas fa-lightbulb"></i>
+									</span><!--//card-icon-holder-->
+									<span class="card-title-text">FAQs</span>
+								</h5>
+								<div class="card-text">
+									Vous vous posez une question ? Consultez la FAQ pour y trouver potentiellement la réponse !
+								</div>
+								<a class="card-link-mask" href="docs-page.php#section-faq"></a>
+							</div><!--//card-body-->
+						</div><!--//card-->
+					</div><!--//col-->
+				</div><!--//row-->
+			</div><!--//container-->
+		</div><!--//container-->
+	</div><!--//page-content-->
+
+	<section class="cta-section text-center py-5 theme-bg-dark position-relative">
+		<div class="theme-bg-shapes-right"></div>
+		<div class="theme-bg-shapes-left"></div>
+		<div class="container">
+			<h3 class="mb-2 text-white mb-3">Launch Your Software Project Like A Pro</h3>
+			<div class="section-intro text-white mb-3 single-col-max mx-auto">Want to launch your software project and
+				start getting traction from your target users? Check out our premium <a class="text-white" href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/">Bootstrap
+					5 startup template CoderPro</a>! It has everything you need to promote your product.</div>
+			<div class="pt-3 text-center">
+				<a class="btn btn-light" href="https://themes.3rdwavemedia.com/bootstrap-templates/startup/coderpro-bootstrap-5-startup-template-for-software-projects/">Get
+					CoderPro<i class="fas fa-arrow-alt-circle-right ml-2"></i></a>
+			</div>
+		</div>
+	</section><!--//cta-section-->
+
+	<footer class="footer">
+
+		<div class="footer-bottom text-center py-5">
+
+			<ul class="social-list list-unstyled pb-4 mb-0">
+				<li class="list-inline-item"><a href="#"><i class="fab fa-github fa-fw"></i></a></li>
+				<li class="list-inline-item"><a href="#"><i class="fab fa-twitter fa-fw"></i></a></li>
+				<li class="list-inline-item"><a href="#"><i class="fab fa-slack fa-fw"></i></a></li>
+				<li class="list-inline-item"><a href="#"><i class="fab fa-product-hunt fa-fw"></i></a></li>
+				<li class="list-inline-item"><a href="#"><i class="fab fa-facebook-f fa-fw"></i></a></li>
+				<li class="list-inline-item"><a href="#"><i class="fab fa-instagram fa-fw"></i></a></li>
+			</ul><!--//social-list-->
+
+			<!--/* This template is free as long as you keep the footer attribution link. If you'd like to use the template without the attribution link, you can buy the commercial license via our website: themes.3rdwavemedia.com Thank you for your support. :) */-->
+			<small class="copyright">Designed with <span class="sr-only">love</span><i class="fas fa-heart" style="color: #fb866a;"></i> by <a class="theme-link" href="http://themes.3rdwavemedia.com" target="_blank">Xiaoying Riley</a> for developers</small>
+		</div>
+
+	</footer>
+	<?php include __DIR__ . '/assets/html/footer.html'; ?>
+</body>
+
+</html>
